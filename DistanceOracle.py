a79137871a5ed25cceff2b30d2295cd30a50a2d1
Zeydel <Seydel14>
2023-03-08 18:44:42 +0100

    cleanup

---
+++ DistanceOracle.py
@@ -0,0 +1,468 @@
+import sys
+import pickle
+import os
+import time
+import numpy as np
+import heapq as heap
+import matplotlib.pyplot as plt
+import scipy.stats as st
+from queue import PriorityQueue
+from random import sample
+from random import random
+from collections import defaultdict
+from itertools import combinations
+from tqdm import tqdm
+
+
+
+
+
+class Node:
+    
+    def __init__(self, node_id):
+        self.id = node_id
+        self.edges = dict()
+        
+    def __str__(self):
+        return f"{self.id}: " + ", ".join([f"({k: v)}" for k,v in self.edges])
+        
+    def get_id(self):
+        return self.id
+    
+    def get_neighbors(self):
+        return [n for n in self.edges]
+    
+    def get_weight(self, neighbor):
+        return self.edges[neighbor]
+        
+    def add_edge(self, neighbor, weight):
+        self.edges[neighbor] = weight
+        
+class Graph:
+    
+    def __init__(self):
+        self.nodes = dict()
+        
+    def add_node(self, node_id):
+        if node_id in self.nodes:
+            return
+        
+        node = Node(node_id)
+        self.nodes[node_id] = node
+        
+    def get_node(self, node_id):
+        return self.nodes[node_id]
+    
+    def add_edge(self, src, dst, cost):
+        if src not in self.nodes:
+            self.add_node(src)
+        if dst not in self.nodes:
+            self.add_node(dst)
+        
+        self.nodes[src].add_edge(self.nodes[dst], cost)
+        self.nodes[dst].add_edge(self.nodes[src], cost)
+        
+    def get_nodes(self):
+        return [k for k in self.nodes]
+    
+    
+def parse(filename='input.txt'):
+    f = open(filename, 'r')
+    text = f.read().strip().split('\n')
+    
+    G = Graph()
+    
+    for l in text:
+        split = l.split()
+        
+        G.add_node(split[0])
+        
+        for e in split[1:]:
+            n, w = e.split(':')
+            
+            G.add_edge(split[0], n, int(w))
+    
+    return G
+
+# Implementation of Dijkstras Algorithm
+def get_min_dist(graph, node):
+    dists = dict()
+    
+    for g in graph.nodes:
+        dists[g] = float('inf')
+        
+    dists[node] = 0
+        
+    queue = PriorityQueue()
+    
+    queue.put((0, node))
+    
+    while not queue.empty():
+        
+        cur_dist, cur_node = queue.get()
+        
+        if cur_dist > dists[cur_node]:
+            continue
+        
+        for neighbor in graph.get_node(cur_node).edges:
+            cost = graph.get_node(cur_node).edges[neighbor]
+            
+            dist = cur_dist + cost
+            
+            if dist < dists[neighbor.get_id()]:
+                dists[neighbor.get_id()] = dist
+                queue.put((dist, neighbor.get_id()))
+        
+        
+    return dists
+
+def get_most_central_nodes(graph, Ai, num_nodes, avg_dists = None):
+
+    if avg_dists == None:
+        avg_dists = dict()
+        for n in tqdm(Ai):
+            
+            node_dists = get_min_dist(graph, n)
+            
+            avg_dists[n] = sum([node_dists[v] for v in node_dists])/len(graph.get_nodes())
+        
+        
+    return (set(sorted(Ai, key = lambda n: avg_dists[n])[:num_nodes]), avg_dists)
+        
+def get_or_create_avg_dists(graph):
+    if os.path.isfile('avg_dists_line.pickle'):
+        return pickle.load(open('avg_dists_line.pickle', 'rb'))
+    else:
+        avg_dists = dict()
+        for n in tqdm(graph.get_nodes()):
+            
+            node_dists = get_min_dist(graph, n)
+            
+            avg_dists[n] = sum([node_dists[v] for v in node_dists])/len(graph.get_nodes())
+        
+        pickle.dump(avg_dists, open('avg_dists_line.pickle', 'wb'))
+        return avg_dists
+
+# Preprocess a graph into a Distance Oracle according to the algorithm
+# described by Thorup and Zwick
+def preprocess(G, k = 3):
+    
+    # Get the nodes from the graph
+    nodes = G.get_nodes()
+    n = len(nodes)
+  
+    # First, create the subsets of decreasing size
+    A = list()
+    
+    # A[0] contains every vertex in the graph
+    A.append(set(nodes))
+    
+    #avg_dists = get_or_create_avg_dists(G)
+    # A[1] to A[k-1] contains every element of the previous set with
+    # probability n^(-1/k)
+    for i in range(1, k):
+        # Use a binomial distribution to determine the size of the sampling
+        sample_size = np.random.binomial(len(A[i-1]), len(A[0])**(-1/(k)))
+        
+        # Use builtin method to pick random vertices from previous sets
+        #A.append(set(sample(A[i-1], sample_size)))
+        
+        # Get the most connected vertices
+        prev = sorted(A[i-1], key=lambda x: len(G.get_node(x).get_neighbors()))
+        A.append(set(prev[-sample_size:]))
+        
+        # Get the most central vertices
+        #A.append(set(sorted(A[i-1], key = lambda n: avg_dists[n])[:sample_size]))
+        
+        # Find j centers
+# =============================================================================
+#         Ai = set()
+#         cur = sample(A[i-1], 1)[0]
+#         Ai.add(cur)
+#         dists = {key: float('inf') for key in A[i-1]}
+#         while len(Ai) < sample_size:
+#             
+#             for key, v in get_min_dist(G, cur).items():
+#                 
+#                 if key in dists: 
+#                     dists[key] = min(dists[key], v)
+#             
+#             
+#             max_dist = float('-inf')
+#             cur = None
+#             for key, v in dists.items():
+#                 if v > max_dist:
+#                     max_dist = v
+#                     cur = key
+#             
+#             Ai.add(cur)
+#             
+#         A.append(Ai)
+# =============================================================================
+            
+        
+    
+
+    # A[k] is the empty set
+    A.append(set())
+    #del avg_dists
+    
+    # If we have zero nodes in the k-1'th set, abort
+    if len(A[k-1]) == 0:
+        raise Exception('The k\'th set was empty. How unfortunate')
+    
+    # Init vars for the oracle
+    delta = defaultdict(lambda: float('inf'))
+    delta_Ai = [dict() for i in range(k+1)]
+    p = [dict() for i in range(k+1)]
+    C = {v: set() for v in G.get_nodes()}
+    B = {v: set() for v in G.get_nodes()}
+    
+    
+    for v in G.get_nodes():
+        # Every node has a distance of zero to itself
+        delta[(v,v)] = 0
+        
+        # And a distance of infinity to the closest node i A[k]
+        delta_Ai[k][v] = float('inf')
+    
+    # Now, for every k-1 to 0 in descending order
+    for i in range(k-1, -1, -1):
+        
+        # Calculate delta(Ai, v) and P(v) for every v
+        delta_Ai_i, p_i = get_p(G, A[i])
+        
+        delta_Ai[i] = delta_Ai_i
+        p[i] = p_i
+        
+        # Get the cluster for every node and calculate distances
+        delta_i, C_i = get_clusters(G, A, C, delta, delta_Ai, i)
+        
+        delta |= delta_i
+        C |= C_i
+        
+    # Compute the bunch for every node
+    for v in C:
+        for w in C[v]:
+            B[w].add(v)
+            
+    # Return the oracle
+    return (dict(delta), B, p)
+        
+            
+# Function for getting closets sampled node in a layer to every node
+def get_p(G, A_i):
+        
+    # Init priority queue
+    queue = []
+    
+    # Set to maintian seen nodes
+    seen = set()
+    
+    # Init the vars to save info
+    delta_Ai_i = {v: float('inf') for v in G.get_nodes()}
+    p_i = dict()
+    
+    # Add all nodes sampled in the layer to the priority queue
+    for n in A_i:
+        heap.heappush(queue, (0, n))
+        # Every sampled node has delta_Ai = 0
+        delta_Ai_i[n] = 0
+        p_i[n] = n
+        
+
+    # Run a dijkstra from all nodes in A_i to find closest sampled nodes to
+    # all vertices
+    while len(queue) > 0:
+        
+        cur_dist, cur_node = heap.heappop(queue)
+        seen.add(cur_node)
+        
+        for n in G.get_node(cur_node).edges:
+            if n.get_id() in seen:
+                continue
+            
+            new_dist = cur_dist + G.get_node(cur_node).edges[n]
+            
+            if new_dist < delta_Ai_i[n.get_id()]:
+                delta_Ai_i[n.get_id()] = new_dist
+                p_i[n.get_id()] = p_i[cur_node]
+                heap.heappush(queue, (new_dist, n.get_id()))
+           
+    return (delta_Ai_i, p_i)
+
+
+# Feeling cute, might delete later
+def get_bunches(G, delta_Ai, A, delta, B, i):
+    
+    queue = []
+    
+    for n in tqdm(A[i] - A[i+1]):
+        queue.put((0, n))
+        seen = set()
+        
+        while not queue.empty():
+            cur_dist, cur_node = queue.get()
+            
+            for v in G.get_node(cur_node).get_neighbors():
+                new_dist = delta[(n, cur_node)] + G.get_node(cur_node).edges[v]
+                
+                if new_dist < delta_Ai[i+1][v.get_id()]:
+                    best = delta[(n, v.get_id())]
+                    
+                    if new_dist < best:
+                        delta[(n, v.get_id())] = new_dist
+                        B[v.get_id()].add(n)
+                        
+                        if v.get_id() not in seen:
+                            queue.put((new_dist, v.get_id()))
+    return (delta, B)
+
+# Function to get clusters for every node in a layer
+def get_clusters(G, A, C, delta, delta_Ai, i):
+    
+    # Init queue
+    queue = []
+    
+    # For every sampled node, not in the next layer
+    for w in tqdm(A[i] - A[i+1]):
+        
+        
+        heap.heappush(queue, (0, w))
+        seen = set()
+        
+        # Compute the cluster
+        while len(queue) > 0:
+            cur_dist, cur_node = heap.heappop(queue)
+            
+            seen.add(cur_node)
+        
+            for v in G.get_node(cur_node).get_neighbors():
+                new_dist = delta[(w, cur_node)] + G.get_node(cur_node).edges[v]
+                
+                if v.get_id() in seen:
+                    continue
+                
+                # Only continue if new_dist is smaller than delta(A_i+1, v)
+                if new_dist > delta_Ai[i+1][v.get_id()] or new_dist >= delta[(w, v.get_id())]:
+                    continue
+                
+                delta[(w, v.get_id())] = new_dist
+                C[w].add(v.get_id())
+                
+                heap.heappush(queue, (new_dist, v.get_id()))
+    
+    return (delta, C)
+ 
+def save_data(delta, B, p):
+
+    file_name = 'pickle.pkl'       
+    with open(file_name, "wb") as file:
+        pickle.dump((delta, B, p), file)
+    
+
+def load_data():
+    
+    file_name = 'pickle.pkl'
+    with open(file_name, 'rb') as file:
+        delta, B, p = pickle.load(file)
+        
+    return (delta, B, p)
+
+def query(B, delta, p, u, v):
+    w = u
+    i = 0
+    
+    while w not in B[v]:
+        i += 1
+        u, v = v, u
+        w = p[i][u]
+        
+    return delta[(w,u)] + delta[(w, v)]
+    
+#Freedman–Diaconis
+def get_number_of_bins(factors):
+    
+    q25, q75 = np.percentile(factors, [25, 75])
+    bin_width = 2 *(q75 - q25) * len(factors) ** (-1/3)
+    return round((max(factors) - min(factors)) / bin_width)
+    
+def test_something(test_input, k):
+    
+    A = list()
+    A.append(set(test_input))
+    
+    for i in range(1, k):
+        Ai = set()
+        for a in A[i-1]:
+            if random() < len(test_input)**((-1)/k):
+                Ai.add(a)
+        A.append(Ai)
+    return A
+
+def test_something_2(test_input, k):
+
+    A = list()
+    A.append(set(test_input))
+    
+    for i in range(1, k):
+        sample_size = np.random.binomial(len(A[i-1]), len(test_input)**(-1/(k)))
+            
+        # Use builtin method to pick random vertices from previous sets
+        A.append(set(sample(A[i-1], sample_size)))
+    return A
+          
+
+
+mem_use = []
+time_use = []
+G = parse("input.txt")
+for k in range(2, 6):
+    #time_start = time.time()
+    delta, B, p = preprocess(G, k)
+    #time_end = time.time()
+    delta = dict(delta)
+    #delta, B, p = load_data()
+    
+    save_data(delta, B, p)
+        
+    #mem_use.append(sys.getsizeof(delta) + sys.getsizeof(B) + sys.getsizeof(p))
+    #time_use.append(time_end - time_start)
+    
+    
+    appx_factors = []
+    
+    for _ in tqdm(range(10000)):
+        u, v = sample(G.get_nodes(), 2)
+    
+        dists = get_min_dist(G, u)    
+        
+        approx = query(B, delta, p, u, v)
+        
+        appx_factors.append(approx/dists[v])
+        
+        if approx/dists[v] > (2*k)-1:
+            print(u)
+            print(v)
+            print(approx/dists[v])
+        
+    #print(stretchSum / len(list(combinations(G.get_nodes(), 2))))
+    #print(stretchSum / 10000)   
+    
+    flierprops = dict(marker='o', markerfacecolor=(0.77, 0, 0.05))
+    medianprops = dict(color=(0.77, 0, 0.05))
+    meanlineprops = dict(linestyle='-', color=(0.12, 0.24, 1))
+    
+    plt.boxplot(appx_factors, flierprops=flierprops, medianprops=medianprops, meanprops=meanlineprops, showmeans=True, meanline=True)
+    plt.title(f'Erdős–Rényi, Sample by Degree, k={k}')
+    plt.savefig(f'Box, ErdosRenyi, Sample by Degree, k={k}.png', bbox_inches='tight')
+    plt.show()
+    plt.hist(appx_factors, bins=get_number_of_bins(appx_factors), label = 'Approximation Factors', color=(0.77, 0, 0.05))
+    mn, mx = plt.xlim()
+    plt.xlim(mn, mx)
+    plt.legend(loc = 'upper right')
+    plt.xlabel('Approximation Factors')
+    plt.title(f'Erdős–Rényi, Sample by Degree, k={k}')
+    plt.savefig(f'Hist, Erdos–Renyi, Sample by Degree, k={k}.png', bbox_inches='tight')
+    plt.show()
+
---
+++ InputGenerator.py
@@ -0,0 +1,260 @@
+import random
+import math
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from random import sample
+from scipy.spatial import Delaunay
+
+#Freedman–Diaconis
+def get_number_of_bins(factors):
+    
+    q25, q75 = np.percentile(factors, [25, 75])
+    bin_width = 2 *(q75 - q25) * len(factors) ** (-1/3)
+    return round((max(factors) - min(factors)) / bin_width)
+    
+
+def generateId(last=''):
+    
+    if last == '':
+        return 'A'
+    
+    if ord(last[-1]) < ord('Z'):
+        return last[:-1] + chr(ord(last[-1])+1)
+    
+    return generateId(last[:-1]) + 'A'
+
+def getDistance(src, dst):
+    if len(src) == 3:
+        return math.sqrt((src[0]-dst[0])**2 + (src[1]-dst[1])**2 + (src[2]-dst[2])**2)
+    
+    elif len(src) == 2:
+        return math.sqrt((src[0]-dst[0])**2 + (src[1]-dst[1])**2)
+
+def generateInput(num_nodes, size, seed=42, connectivity=0.5):
+    
+    f = open("input.txt", 'a')
+    
+    random.seed(seed)
+    
+    nodes = set()
+    
+    last = ''
+    neighbors = dict()
+    
+    for n in tqdm(range(num_nodes)):
+        
+        x = random.randint(0, size)
+        y = random.randint(0, size)
+        z = random.randint(0, size)
+        node_id = generateId(last)
+        last = node_id
+        
+        node = (node_id, x, y, z)
+        neighbors[node_id] = 0
+        string = node[0]
+        
+        for dst in nodes:
+            
+            if random.random() <= connectivity:
+                
+                string += ' ' + dst[0] + ':' + str(round(getDistance(node[1:], dst[1:])))
+                neighbors[node[0]] += 1
+                neighbors[dst[0]] += 1
+                
+        nodes.add(node)
+                
+                
+        f.write(string + '\n')
+        
+        
+    return neighbors
+
+def generateClusteredGraph(num_nodes, num_edges, size, seed=42):
+    
+    nodes = dict()
+    node_coordinates = dict()
+    
+    random.seed(seed)
+
+    last = ''
+    
+    for _ in tqdm(range(num_nodes)):
+        
+        x = random.randint(0, size)
+        y = random.randint(0, size)
+        z = random.randint(0, size)
+        node_id = generateId(last)
+        last = node_id
+        
+        node_coordinates[node_id] = (x, y, z)
+        nodes[node_id] = set()
+        
+    unvisited = set(nodes)
+    
+    cur = sample(unvisited, 1)[0]
+    unvisited.remove(cur)
+    cur_edges = 0
+    
+    while len(unvisited) > 0:
+        
+        neighbor = sample(unvisited, 1)[0]
+        unvisited.remove(neighbor)
+        
+        nodes[cur].add(neighbor)
+        nodes[neighbor].add(cur)
+        cur_edges += 1
+        
+        cur = neighbor
+        
+    choices = list(nodes)
+    total_degree = sum([len(nodes[n]) for n in nodes])
+    
+    while cur_edges < num_edges:
+        
+        weights = [len(nodes[n])/total_degree for n in choices]
+        
+        u, v = np.random.choice(choices, 2, p=weights, replace=False)
+        u, v = str(u), str(v)
+        
+        if u in nodes[v] or v in nodes[u]:
+            continue
+        
+        nodes[u].add(v)
+        nodes[v].add(u)
+        cur_edges += 1
+        total_degree += 2
+        
+    f = open("input.txt", "a")    
+    
+    for u in nodes:
+        
+        string = u
+        
+        for v in nodes[u]:
+            string += ' ' + v + ':' + str(round(getDistance(node_coordinates[u], node_coordinates[v])))
+            
+        f.write(string + '\n')    
+        
+    return nodes
+
+def generatePlanarGraph(num_nodes, size):
+    
+    node_coordinates = dict()
+    nodes = dict()
+    
+    last = ''
+    
+    for n in range(num_nodes):
+        
+        x = random.randint(0, size)
+        y = random.randint(0, size)
+        
+        node_coordinates[n] = (x, y)
+        
+    tri = Delaunay([node_coordinates[n] for n in node_coordinates])
+    
+    for t in tri.simplices:
+        
+        if int(t[0]) not in nodes:
+            nodes[int(t[0])] = set()
+        
+        if int(t[1]) not in nodes:
+            nodes[int(t[1])] = set()
+        
+        if int(t[2]) not in nodes:
+            nodes[int(t[2])] = set()
+        
+        nodes[int(t[0])].add(int(t[1]))
+        nodes[int(t[0])].add(int(t[2]))
+        
+        nodes[int(t[1])].add(int(t[0]))
+        nodes[int(t[1])].add(int(t[2]))
+        
+        nodes[int(t[2])].add(int(t[0]))
+        nodes[int(t[2])].add(int(t[1]))
+        
+        
+    f = open("input.txt", "a")    
+    
+    for u in nodes:
+        string = str(u)
+        
+        for v in nodes[u]:
+            
+            string += ' ' + str(v) + ':' + str(round(getDistance(node_coordinates[u], node_coordinates[v])))
+            
+        f.write(string + '\n')    
+        
+    return nodes
+
+    
+
+def generateLineGraph(num_nodes):
+    
+    f = open("input.txt", "a")
+    
+    last = ''
+    
+    node_id = generateId()
+    
+    f.write(node_id + '\n')
+    
+    for n in range(1, num_nodes):
+        
+        last = node_id
+        
+        node_id = generateId(node_id)
+        f.write(node_id + ' '  + last + ":1\n")
+   
+def generateRoadGraph():
+    
+    edges = open("CAL_Edges.cedge").read().split('\n')
+    
+    f = open("input_roads.txt", "a")
+    
+    dists = dict()
+    nodes = dict()
+    for e in edges:
+        
+        _, n1, n2, d = e.split(' ')
+        
+        if n1 not in nodes:
+            nodes[n1] = 0
+        
+        if n2 not in nodes:
+            nodes[n2] = 0
+            
+        nodes[n1] += 1
+        nodes[n2] += 1
+        
+        if n1 not in dists:
+            dists[n1] = dict()
+            
+        dists[n1][n2] = d.replace('.', '')
+        
+    for n1 in dists:
+        string = n1
+        
+        for n2 in dists[n1]:
+            string += ' ' + n2 + ":" + dists[n1][n2]
+            
+        f.write(string + '\n')
+
+    return nodes
+        
+        
+if os.path.exists("input.txt"):
+    os.remove("input.txt")
+
+nodes = generateInput(1000, 1000, connectivity=0.05)
+
+
+plt.hist([nodes[n] for n in nodes], label = 'Approximation Factors', color=(0.77, 0, 0.05))
+mn, mx = plt.xlim()
+plt.xlim(mn, mx)
+plt.legend(loc = 'upper right')
+plt.xlabel('Vertex Degree')
+plt.title(f'Californian Roads')
+plt.show()
